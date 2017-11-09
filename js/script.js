4ecf067ef3f42c2b779ddb8b9e22a50995b92687
sudhir <sudhir.kulat@springernature.com>
2017-11-09 14:32:07 +0530

    Carousel mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 
- $('.menu-icon').click(function(){
+  $('.menu-icon').click(function(){
 	this.classList.toggle("cross");
     $('.mob-menu').toggle('linear');
  });
+ 
+ var slideIndex = 1;
+showSlide(slideIndex);
+
+function plusSlides(n){
+showSlide(slideIndex += n);
+}
+
+function currentSlide(n) {
+showSlide(slideIndex = n);
+}
+
+function showSlide(n){
+var i;
+var slides = document.getElementsByClassName("slider");
+var dots = document.getElementsByClassName("dots");
+if (n > slides.length) 
+    { 
+        slideIndex = 1
+};
+if (n < 1) { 
+    slideIndex = slides.length
+};
+
+for (i=0;i<slides.length;i++) {
+slides[i].style.display = "none";
+};
+for (i=0;i<dots.length;i++) {
+dots[i].className = dots[i].className.replace("active","");
+};
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+}
